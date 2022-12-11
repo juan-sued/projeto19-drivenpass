da5c2a48c7ca2c141b9c1c8c6881636a38ff1593
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-12-11 04:15:16 -0300

    implemented login route

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,18 +1,30 @@
 import { User } from '@prisma/client';
 
-export type ApplicationError = {
-  name: string;
-  message: string;
-};
+//========= create user types ===========//
 
 export type CreateUserParams = Pick<User, 'email' | 'password'>;
 
-//Regra de Negócio
+//========= sign-in types ===========//
+
+export type SignInParams = Pick<User, 'email' | 'password'>;
+
+export type SignInResult = {
+  token: string;
+};
+
+export type GetUserOrFailResult = Pick<User, 'id' | 'email' | 'password'>;
+
+//========= errors types ===========//
 
 export type RequestError = {
   status: number;
   data: object | null;
   statusText: string;
+  name: string;
+  message: string;
+};
+
+export type ApplicationError = {
   name: string;
   message: string;
 };
